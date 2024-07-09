8cac38c8f100bc00dfc7ecd889b86cc76537dbc1
Sanketh Shinde <sankethshinde99@gmail.com>
2024-07-09 19:01:08 +0530

    create soap api

---
+++ src/test/java/com/eidiko/soap/CreateSoapApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.eidiko.soap;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CreateSoapApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
